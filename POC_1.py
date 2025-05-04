d5b9f44f7c7ee396f7f5ad3942ccdd6b28f9bfc0
Bhushan Junagade <137473467+bhushanio@users.noreply.github.com>
2025-05-04 08:25:47 +0530

    Updated script POC_1.py

--- POC_1.py
+++ POC_1.py
@@ -1,1 +1,9 @@
-print("HI THIS IS A JENKINS JOB")
+import webbrowser
+import os
+
+# Define the HTML file path
+html_file = "hacked_prank_fixed.html"
+
+# Open the HTML file in the default browser
+file_url = f"file://{os.path.abspath(html_file)}"
+webbrowser.open(file_url)
